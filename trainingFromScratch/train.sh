ea50f0bcea41b707b54c03cb99807f515e576366
Ivan Pereira <navi1921@gmail.com>
2019-10-12 22:53:28 -0300

    Update train.sh

--- trainingFromScratch/train.sh
+++ trainingFromScratch/train.sh
@@ -49,7 +49,7 @@ num_classes=3
 # Freeze the layers before a certain layer.
 freeze_layer=0
 # Batch size
-batchsize=60
+batchsize=32
 # The number of total epochs for training
 epoch=65
 # The inital learning rate
